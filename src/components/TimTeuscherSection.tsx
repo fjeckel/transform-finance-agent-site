45e41b4a977d6a4d742f0463bcd7700b4afd5221
fjeckel <fjeckel@me.com>
2025-06-23 19:54:45 +0200

    Allow profile images to scale larger

--- src/components/TimTeuscherSection.tsx
+++ src/components/TimTeuscherSection.tsx
@@ -54,7 +54,7 @@ const TimTeuscherSection = () => {
           <div className="order-1 lg:order-2">
             <div className="flex flex-col items-center text-center space-y-2">
 
-              <Avatar className="w-40 h-40 rounded-none">
+              <Avatar className="w-40 h-40 max-w-lg rounded-none">
 
                 <AvatarImage src="/img/tim-teuscher.jpg" alt="Tim Teuscher" />
                 <AvatarFallback>TT</AvatarFallback>
